cdabb7e9c4149cae74bf5b56d474bf6ba48c244f
chibaongunguoi <chibaongunguoi@gmail.com>
2024-05-07 15:44:52 +0700

    a

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,11 +35,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
